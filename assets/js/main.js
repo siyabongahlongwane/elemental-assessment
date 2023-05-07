0d8c4b6d75d70aea74b10c823cb7b9aaf34a7807
Siyabonga <hlongwanesiyabonga6@gmail.com>
2023-05-07 16:16:38 +0200

    Carousel functionality

--- assets/js/main.js
+++ assets/js/main.js
@@ -15,26 +15,61 @@
             path: "slide-4.png",
             text: "we leave no stone unturned"
         }
-    ]
+    ];
+
     let folderDir = "../assets/images"
     let slide = $("#slide");
     let slideText = $("#slide-text");
+    let carousel = $("#carousel");
     let slideIndex = 0;
-
     // Set Default Slide Image and Text
-    const setCarouselData = () => {
-        slide.css("background-image", `url(${folderDir}/${carouselData[slideIndex]['path']})`);
+    const setCarouselData = (slideIndex) => {
+        slide.attr("src", `${folderDir}/${carouselData[slideIndex]['path']}`);
         slideText.html(`${carouselData[slideIndex]['text']}`);
     }
 
-    $(document).ready(setCarouselData());
+    $(document).ready(() => {
+        setCarouselData(0);
+        toggleTrackingIcons();
+    });
 
     // Go to the next slide
-    const next = () => {
-        slideIndex = slideIndex == carouselData.length - 1 ? 0 : ++slideIndex;
-        console.log('next slide', slideIndex);
-
-        slide.css("background-image", `url(${folderDir}/${carouselData[slideIndex]['path']})`);
+    const next = (indx) => {
+        if (indx) {
+            slideIndex = indx;
+        } else if (slideIndex == carouselData.length - 1) {
+            slideIndex = 0;
+        } else {
+            slideIndex += 1;
+        }
+        slide.attr("src", `${folderDir}/${carouselData[slideIndex]['path']}`);
         slideText.html(`${carouselData[slideIndex]['text']}`);
-
+        toggleTrackingIcons();
     }
+
+    const toggleTrackingIcons = () => {
+        resetChildElements();
+        carouselData.forEach((currentSlide, index) => {
+            let img = $('<img />', {
+                id: `slide-${index}`,
+                class: index == slideIndex ? 'pointer active-slide' : 'pointer',
+                src: index == slideIndex ? `${folderDir}/active.png` : `${folderDir}/inactive.png`,
+                width: index == slideIndex ? '20px' : '15px',
+            });
+            img.on("click", function() {
+                next(index);
+            });
+            img.appendTo($('#trackerIcons'));
+        })
+    }
+
+    const resetChildElements = () => {
+        let parent = document.querySelector("#trackerIcons");
+        let child = parent.lastElementChild;
+        while (child) {
+            parent.removeChild(child);
+            child = parent.lastElementChild;
+        }
+    }
+
+    document.querySelector("#trackerIcons").addEventListener("click", next)
